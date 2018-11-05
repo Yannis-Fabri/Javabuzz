cd8a27cc134bb76041f1c8fd9a458660f589d409
Yannis-Fabri <yannisk888@gmail.com>
2018-11-05 13:34:43 +0000

    Incorperated a function into the code

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,6 +1,9 @@
-for ( var n = 1; n < 51; n++){
-  if ( n % 15 == 0) console.log('FizzBuzz');
-  else if ( n % 3 == 0) console.log('Fizz');
-  else if ( n % 5 == 0) console.log('Buzz');
-  else console.log(n);
+function fizzbuzz(){
+  for ( var n = 1; n < 51; n++){
+    if ( n % 15 == 0) console.log('FizzBuzz');
+    else if ( n % 3 == 0) console.log('Fizz');
+    else if ( n % 5 == 0) console.log('Buzz');
+    else console.log(n);
+  }
 }
+fizzbuzz();
